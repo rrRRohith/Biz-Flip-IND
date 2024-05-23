69157f00d9a703cb30d863ed0844d1a3f60b1c6b
Shefii-02 <shefii.km@gmail.com>
2024-05-23 19:14:38 +0530

    fourth commit

---
+++ app/Http/Resources/ContentPageResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class ContentPageResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'             => $this->id,
+            'title'          => $this->title,	
+            'slug'           => $this->slug,
+            'content'        => $this->content,	
+            'seo_title'      => $this->seo_title,	
+            'seo_keywords'   => $this->seo_keywords,	
+            'seo_desscription'=> $this->seo_desscription,	
+            'status'         => $this->status == 1 ? 'Published' : 'Draft',
+            'breadcumb_image'=> $this->image && !(str_starts_with($this->breadcumb_image, 'http')) ?
+                                asset('images/'.$this->breadcumb_image) : $this->breadcumb_image,
+            'created_at'     => (new Carbon($this->created_at))->format('Y-m-d'),
+            'updated_at'     => (new Carbon($this->updated_at))->format('Y-m-d'),
+        ];
+    }
+}
