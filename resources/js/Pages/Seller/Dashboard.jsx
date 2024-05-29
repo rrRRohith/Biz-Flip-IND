6617929c7e4a727516e9fb626576398180437984
cinta felixon <cintafcindigital@gmail.com>
2024-05-29 14:37:25 +0530

    change preloader

--- resources/js/Pages/Seller/Dashboard.jsx
+++ resources/js/Pages/Seller/Dashboard.jsx
@@ -9,8 +9,97 @@ export default function Dashboard({ auth }) {
             <Wrapper>
                 <main className="py-6 bg-surface-secondary">
                     <div className="container">
-
-
+                        <div class="row g-6">
+                            <div className="col-xl-3 col-sm-6 col-12">
+                                <div className="card rounded-input">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col">
+                                                <span className="h6 font-semibold text-muted text-sm d-block mb-2">Properties</span>
+                                                <span className="h3 font-bold mb-0">2</span>
+                                            </div>
+                                            <div className="col-auto">
+                                                <div className="icon icon-shape bg-tertiary text-white text-lg rounded-circle">
+                                                    <i className="bi bi-credit-card" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="mt-2 mb-0 text-sm">
+                                            <span className="badge badge-pill bg-soft-success text-success me-2">
+                                                <i className="bi bi-arrow-up me-1" />30% </span>
+                                            <span className="text-nowrap text-xs text-muted">Since last month</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-sm-6 col-12">
+                                <div className="card rounded-input">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col">
+                                                <span className="h6 font-semibold text-muted text-sm d-block mb-2">Leads</span>
+                                                <span className="h3 font-bold mb-0">215</span>
+                                            </div>
+                                            <div className="col-auto">
+                                                <div className="icon icon-shape bg-primary text-white text-lg rounded-circle">
+                                                    <i className="bi bi-people" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="mt-2 mb-0 text-sm">
+                                            <span className="badge badge-pill bg-soft-success text-success me-2">
+                                                <i className="bi bi-arrow-up me-1" />23% </span>
+                                            <span className="text-nowrap text-xs text-muted">Since last week</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-sm-6 col-12">
+                                <div className="card rounded-input">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col">
+                                                <span className="h6 font-semibold text-muted text-sm d-block mb-2">View count</span>
+                                                <span className="h3 font-bold mb-0">2345</span>
+                                            </div>
+                                            <div className="col-auto">
+                                                <div className="icon icon-shape bg-info text-white text-lg rounded-circle">
+                                                    <i className="bi bi-clock-history" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="mt-2 mb-0 text-sm">
+                                            <span className="badge badge-pill bg-soft-danger text-danger me-2">
+                                                <i className="bi bi-arrow-down me-1" />-10% </span>
+                                            <span className="text-nowrap text-xs text-muted">Since last month</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-sm-6 col-12">
+                                <div className="card rounded-input">
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col">
+                                                <span className="h6 font-semibold text-muted text-sm d-block mb-2">Views to lead ratio</span>
+                                                <span className="h3 font-bold mb-0">95%</span>
+                                            </div>
+                                            <div className="col-auto">
+                                                <div className="icon icon-shape bg-warning text-white text-lg rounded-circle">
+                                                    <i className="bi bi-minecart-loaded" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="mt-2 mb-0 text-sm">
+                                            <span className="badge badge-pill bg-soft-success text-success me-2">
+                                                <i className="bi bi-arrow-up me-1" />15% </span>
+                                            <span className="text-nowrap text-xs text-muted">Since yestearday</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
                 </main>
             </Wrapper>
