0f1572c0b657a73671f4e1b0b613b29f70a25250
Shefii-02 <shefii.km@gmail.com>
2024-05-30 12:34:17 +0530

    test

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,6 @@
 // import './bootstrap';
 // import '../css/app.css';
-
+import React,{ useEffect,useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp, router } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
