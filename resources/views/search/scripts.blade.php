9850d6efd867450b248afb81d1ed231b6a73de1e
rrRRohith <rohithr.indigital@gmail.com>
2024-06-25 16:41:49 +0530

    fix country city

--- resources/views/search/scripts.blade.php
+++ resources/views/search/scripts.blade.php
@@ -112,7 +112,12 @@
             }else if(Object.values(store.provinces).includes(true)){
                 const country = store.countries.find(item => item.id == store.country);
                 return country.provinces.filter(province => store.provinces['province__' + province.id]).flatMap(province => province.cities);
-            }else{
+            }
+            else if(store.country != 'all'){
+                const country = store.countries.find(item => item.id == store.country);
+                return country.provinces.flatMap(province => province.cities);
+            }
+            else{
                 return store.adCities;
             }
             
