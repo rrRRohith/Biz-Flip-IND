bbf585dfa12352b52ce7d5441c7da96743ff29d7
Shefii-02 <shefii.km@gmail.com>
2024-08-27 18:42:44 +0530

    added

--- app/Http/Controllers/Admin/BackupController.php
+++ app/Http/Controllers/Admin/BackupController.php
@@ -21,9 +21,9 @@ class BackupController extends Controller
     {
         //    
         $bannersList = Banner::query()->paginate(10);
-            
+                                
         return Inertia::render('Admin/Banner/Index',['bannersList' => BannerResource::collection($bannersList),'success' => session('success'),'error' => session('error')]);
-     
+                  
     }
 
 }
