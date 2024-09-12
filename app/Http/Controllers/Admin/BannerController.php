d52ceb93349d0b7c0b14ea121d4bbb4bed3f5cbe
rrRRohith <rohithr.indigital@gmail.com>
2024-09-12 16:11:44 +0530

    error fix

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -43,7 +43,7 @@ class BannerController extends Controller
         //
         if ($request->picture_desktop) {
             $imagePath = $this->uploadFile(file : $request->picture_desktop, path : 'banners');
-
+        }
         
         if ($request->picture_mobile) {
             $imagePath2 = $this->uploadFile(file : $request->picture_mobile, path : 'banners');
