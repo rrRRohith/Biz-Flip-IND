da6c8e958f2ffb04369a193e82da43b342f49792
rrRRohith <rohithr.indigital@gmail.com>
2024-09-17 15:46:56 +0530

    profile links fix

--- resources/js/Pages/Seller/Components/ProfileDropdown.jsx
+++ resources/js/Pages/Seller/Components/ProfileDropdown.jsx
@@ -15,26 +15,38 @@ export default function ProfileDropdown({ user }) {
                 <span className="d-block text-heading font-semibold">{user.firstname} {user.lastname}</span>
             </div>
             <div className="dropdown-divider"></div>
-            <Link className="dropdown-item" href="/seller/profile">
-                <i className="bi bi-pencil me-3" />Profile </Link>
-            <Link className="dropdown-item" href="/seller/password">
-                <i class="bi bi-shield-lock me-3"></i>Security </Link>
-            <PermissionAllow permission="Plan Purchase">
-                <Link className="dropdown-item" href="/seller/plans">
-                    <i class="bi bi-currency-dollar me-3"></i>Plans </Link>
-            </PermissionAllow>
-            <PermissionAllow permission="Invoice List">
-                <Link className="dropdown-item" href="/seller/invoices">
-                    <i class="bi bi-receipt me-3"></i>Invoices </Link>
+            {user.type == 'customer' ? (
+                <>
+                    <Link className="dropdown-item" href="customer/profile">
+                        <i className="bi bi-pencil me-3" />Profile </Link>
+                    <Link className="dropdown-item" href="/customer/password">
+                        <i class="bi bi-shield-lock me-3"></i>Security </Link>
+                </>
+            ) : (
+                <>
+                    <Link className="dropdown-item" href="/seller/profile">
+                        <i className="bi bi-pencil me-3" />Profile </Link>
+                    <Link className="dropdown-item" href="/seller/password">
+                        <i class="bi bi-shield-lock me-3"></i>Security </Link>
+                    <PermissionAllow permission="Plan Purchase">
+                        <Link className="dropdown-item" href="/seller/plans">
+                            <i class="bi bi-currency-dollar me-3"></i>Plans </Link>
+                    </PermissionAllow>
+                    <PermissionAllow permission="Invoice List">
+                        <Link className="dropdown-item" href="/seller/invoices">
+                            <i class="bi bi-receipt me-3"></i>Invoices </Link>
 
-            </PermissionAllow>
-            <PermissionAllow permission="Settings">
-                <Link className="dropdown-item" href="/seller/settings">
-                    <i className="bi bi-gear me-3" />Settings </Link>
-            </PermissionAllow>
-            <div className="dropdown-divider" />
-            <Link className="dropdown-item" onClick={handleClick}>
-                <i className="bi bi-box-arrow-left me-3" />Logout </Link>
+                    </PermissionAllow>
+                    <PermissionAllow permission="Settings">
+                        <Link className="dropdown-item" href="/seller/settings">
+                            <i className="bi bi-gear me-3" />Settings </Link>
+                    </PermissionAllow>
+                    <div className="dropdown-divider" />
+                    <Link className="dropdown-item" onClick={handleClick}>
+                        <i className="bi bi-box-arrow-left me-3" />Logout </Link>
+                </>
+            )}
+
         </div>
     );
 }
