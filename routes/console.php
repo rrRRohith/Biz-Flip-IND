789b492aadce5844c44a3e28e70fef2cb3b9084f
James <rohithr.indigital@gmail.com>
2024-09-19 15:26:28 +0530

    daily backupfix

--- routes/console.php
+++ routes/console.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-    $this->command('backup:google')->hourly();
+    // $this->command('backup:google')->hourly();
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('app:subscription-expire-alert')->daily();
 Schedule::command('queue:work --stop-when-empty')->everyMinute();
+// Schedule::command('backup:google')->hourly();
