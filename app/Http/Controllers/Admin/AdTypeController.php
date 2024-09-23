29ff09fd1681a67829c4864b419e8d284c0ddcb2
James <rohithr.indigital@gmail.com>
2024-09-23 13:50:27 +0530

    ad_type CRUD

--- app/Http/Controllers/Admin/AdTypeController.php
+++ app/Http/Controllers/Admin/AdTypeController.php
@@ -1,11 +1,122 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\AdType;
 use Illuminate\Http\Request;
+use App\Http\Requests\AdType\StoreAdTypeRequest;
+use App\Http\Requests\AdType\UpdateAdTypeRequest;
+use App\Http\Resources\AdTypeResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+use Exception;
 
 class AdTypeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $typeList = AdType::query()->paginate(10);
+       
+        return Inertia::render('Admin/AdTypes/Index',['typeList' => AdTypeResource::collection($typeList),'success' => session('success'),'error' => session('error')]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Admin/AdTypes/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdTypeRequest $request)
+    {
+
+        $new        = new AdType();
+        $new->title  = $request->title;
+        $new->slug  = Str::slug($request->title);
+        $new->icon  = $request->icon ?? null;
+        
+        $new->position=$request->position;
+        $new->status= $request->status;
+        try{
+            $new->save();			
+            return to_route('admin.ad_types.index')->with('success', 'type was created.');
+        }
+        catch(Exception $e){
+            return $e->getMessage();
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AdType $type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AdType $ad_type)
+    {
+        $type = $ad_type;
+      
+        
+        return Inertia::render('Admin/AdTypes/Edit',['type_item' => new AdTypeResource($type),'success' => session('success'),'error' => session('error')]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdTypeRequest $request,$id)
+    {
+        //
+
+      
+
+        $type = AdType::where('id',$id)->first() ?? abort(404);
+        $data = $request->validated();
+        
+
+        $type->title  = $request->title;
+        $type->slug  = Str::slug($request->title);
+        $type->icon  = $request->icon ?? null;
+        $type->position=$request->position;
+        $type->status= $request->status;
+        $type->save();
+
+        return to_route('admin.ad_types.index')
+            ->with('success', "Type \"$type->title\" was updated");
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+   
+        $type = AdType::where('id',$id)->first() ?? abort(404);
+        $name = $type->title;
+        $type->delete();
+        // if ($type->icon) {
+        //     Storage::disk('images')->delete($type->icon);
+           
+        // }
+        return to_route('admin.ad_types.index')
+            ->with('success', "Type \"$name\" was deleted");
+    }
 }
+
+
---
+++ resources/js/Pages/Admin/AdTypes/Index.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Head, Link, router } from '@inertiajs/react';
+import Authenticated from '@/Layouts/AdminAuthenticated';
+import PermissionAllow from '@/Components/PermissionAllow';
+import Swal from 'sweetalert2';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+
+export default function Index({ typeList, auth, success = null, error = null }) {
+     
+    const deletePupose = (type) => {
+      
+        Swal.fire({
+            title: 'Are you sure you want to delete this Ad Type?',
+            text: 'Once deleted, it cannot be recovered.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                router.delete(route("admin.ad_types.destroy", type.id), {
+                    onSuccess: () => {
+                        Swal.fire('Deleted!', 'Ad Type has been deleted.', 'success');
+                    },
+                });
+            }
+        });
+      
+      }
+
+      
+
+    return (
+        <Authenticated
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Ad Type</h2>}
+            success = {success}
+            error   = {error}
+        >
+            <Head title="Ad Type List" />
+
+            {/* <!-- Content Wrapper. Contains page content --> */}
+            <div className="content-wrapper me-4">
+                <div className="container-full">
+                    {/* <!-- Content Header (Page header) --> */}
+                    <div className="content-header">
+                        <div className='row'>
+                            <div className='col-lg-6'>
+                                <div className="d-flex align-items-center">
+                                    <div className="me-auto">
+                                        <h4 className="page-title">Ad types</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-lg-6'>
+                                <div className="text-end">
+                                <PermissionAllow permission={'Ad Type Create'}>
+                                <Link className='btn btn-info text-end' href={route('admin.ad_types.create')}><i className='bi bi-plus'></i> Create</Link>
+                                </PermissionAllow>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* <!-- Main content --> */}
+                    <section className="content">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="box">
+                                    <div className="box-body">
+                                    <PermissionAllow permission={'Ad Type Listing'} message={true}>
+                                        <div className="table-responsive rounded card-table">
+                                            <Table className="table border-no" id="example1">
+                                                <Thead>
+                                                    <Tr>
+                                                        <Th>#</Th>
+                                                        <Th>Name</Th>
+                                                        <Th>Position</Th>
+                                                        <Th>Status</Th>
+                                                        <Th>Last Modified</Th>
+                                                        <Th></Th>
+                                                    </Tr>
+                                                </Thead>
+                                                <Tbody>
+
+                                                {typeList.data.map((type) => (
+                                                    <Tr key={type.id} className="hover-primary">
+                                                        <Td>{type.id}</Td>
+                                                        <Td>
+                                                            <i className={`${type.icon} bi  w-40 rounded-5`}></i>
+                                                            <span className='ms-3'>{type.title}</span>
+                                                        </Td>
+                                                        <Td>{type.position}</Td>
+                                                        <Td>{type.status}</Td>
+                                                        <Td>{window.formatDateTime(type.updated_at)}</Td>
+                                                        <Td>
+                                                            <PermissionAllow permission={'Ad Type Edit'}>
+                                                                <Link className='btn btn-transparent' href={route('admin.ad_types.edit', type.id)}>
+                                                                    <i className="bi bi-pencil"></i>
+                                                                </Link>
+                                                            </PermissionAllow>
+                                                            <PermissionAllow permission={'Ad Type Delete'}>
+                                                                <button onClick={(e) => deletePupose(type)} className="btn btn-transparent border-0">
+                                                                    <i className="bi bi-trash"></i>
+                                                                </button>
+                                                            </PermissionAllow>
+                                                        </Td>
+                                                    </Tr>
+                                                ))}
+
+                                                </Tbody>
+                                            </Table>
+                                        </div>
+                                    </PermissionAllow>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    {/* <!-- /.content --> */}
+
+                </div>
+            </div>
+            {/* <!-- /.content-wrapper --> */}
+        </Authenticated>
+
+    )
+}
