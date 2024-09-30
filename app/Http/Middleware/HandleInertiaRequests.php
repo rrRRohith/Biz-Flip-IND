966a95f487c02beefe2bedf93346ae990c494513
James <rohithr.indigital@gmail.com>
2024-09-30 14:01:57 +0530

    currency_code fix

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -55,7 +55,7 @@ class HandleInertiaRequests extends Middleware
             'captcha_validation_disable' => env('CAPTCHA_VALIDATION_DISABLE', false),
             'notifications' => $request->user() ? DashboardNotification::where('recipient_id', $request->user()->id)->whereNull('read_at')->limit(6)->get() : [],
             'menu' => $menu ? $menu->childs->toArray() : [],
-            'currency_code' => env('CURRENCY_CODE')
+            'currency_code' => 'INR',//env('CURRENCY_CODE')
         ];
     }
 
