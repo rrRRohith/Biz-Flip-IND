6b363734546b764815b1f46a02f7b60e55430394
James <rohithr.indigital@gmail.com>
2024-10-01 14:10:42 +0530

    phone mask

--- app/Http/Requests/SellerUpdateStep3Request.php
+++ app/Http/Requests/SellerUpdateStep3Request.php
@@ -26,25 +26,33 @@ class SellerUpdateStep3Request extends FormRequest
     public function rules(): array
     {
         return [
-            'picture' => ['nullable', 'image'],
-            'company_name' => ['required', 'string', 'max:255'],
+            // 'picture' => ['nullable', 'image'],
+            // 'company_name' => ['required', 'string', 'max:255'],
             // 'firstname' => ['required', 'string', 'max:255'],
             // 'lastname' => ['required', 'string', 'max:255'],
             //'short_description' => ['required', 'string', 'max:255'],
-            'address' => ['required', 'string', 'max:255'],
-            'employee' => ['required', 'string', 'max:255'],
-            'description' => ['required', 'string'],
-            'phone' => ['required', 'numeric', 'digits:10'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255'],
-            'website' => ['sometimes', 'nullable', 'string'],
-            'days' => 'sometimes|nullable|array',
-            'address' => 'required|max:256|string',
-            'city' => 'required|max:256|string',
-            'postalcode' => 'required|max:10|string',
-            'province' => 'required|exists:provinces,name',
-            'location.lat' => 'sometimes|nullable|max:256|string',
-            'location.lng' => 'sometimes|nullable|max:256|string',
-            'established' => 'required|digits:4',
+            // 'address' => ['required', 'string', 'max:255'],
+            // 'employee' => ['required', 'string', 'max:255'],
+            // 'description' => ['required', 'string'],
+            // 'phone' => ['required', 'numeric', 'digits:10'],
+            // 'email' => ['required', 'string', 'lowercase', 'email', 'max:255'],
+            // 'website' => ['sometimes', 'nullable', 'string'],
+            // 'days' => 'sometimes|nullable|array',
+            // 'address' => 'required|max:256|string',
+            // 'city' => 'required|max:256|string',
+            // 'postalcode' => 'required|max:10|string',
+            // 'province' => 'required|exists:provinces,name',
+            // 'location.lat' => 'sometimes|nullable|max:256|string',
+            // 'location.lng' => 'sometimes|nullable|max:256|string',
+            // 'established' => 'required|digits:4',
         ];
+        
     }
+
+    // public function getValidatorInstance(){
+    //     $this->merge([
+    //         'phone' => str_replace('-','',filter_var($this->phone, FILTER_SANITIZE_NUMBER_INT)),
+    //     ]);
+    //     return parent::getValidatorInstance();
+    // }
 }
