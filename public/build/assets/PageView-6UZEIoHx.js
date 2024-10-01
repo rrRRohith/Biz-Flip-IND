e3044ce79eeccdeb4e475bbe972ea58a12353500
James <rohithr.indigital@gmail.com>
2024-10-01 13:04:06 +0530

    build

--- public/build/assets/PageView-6UZEIoHx.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-BcN2wwfP.js";const t=({collection:s,handleClose:n})=>e.jsx("div",{className:"col-lg-12",children:e.jsx("div",{dangerouslySetInnerHTML:{__html:s.content}})}),a=t;export{a as default};
